27e5f711b2b086109ac098d784e40f6aa792c9a4
pd-9999 <51860817+pd-9999@users.noreply.github.com>
2019-06-17 16:16:45 +0100

    added data structures but not node

--- Neat.lua
+++ Neat.lua
@@ -24,3 +24,65 @@ c2 = 1
 c3 = 0.4
 deltaT = 3
 staleLim = 15
+
+
+--Data structures
+
+function makeGene()
+
+  local gene = {}
+
+  gene.I = 0
+  gene.O = 0
+  gene.weight = 0
+  gene.enable = 1
+  gene.innovation = 1 --Might need change
+
+  return gene
+
+end
+
+
+function makeGenome()
+
+  local genome = {}
+
+  genome.genes = {}
+  genome.speciesRank = 0
+  genome.globalRank = 0
+  genome.fitness = 0
+
+  return genome
+
+end
+
+
+function makeSpecies()
+
+  local species
+
+  species.genomes = {}
+  species.sumADJFit = 0
+  species.numTOkill = 0
+  species.staleness = 0
+  species.example = {}
+
+  return species
+
+end
+
+
+function makeGen()
+
+  local gen = {}
+
+  gen.number = 0
+  gen.species = {}
+  gen.maxFitness = 0
+
+  return gen
+
+end
+
+
+--
